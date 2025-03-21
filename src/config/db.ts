b4ce8b4798d8b0a3b54f793cff9d3c2207bd6554
ShaggyMunky <aaronguillen21@gmail.com>
2025-03-21 02:25:18 -0700

    connection configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,28 +1,28 @@
-import sql from 'mssql';
-import dotenv from 'dotenv';
+import sql from "mssql";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const dbConfig: sql.config = {
-    server: process.env.DB_SERVER as string,
-    database: process.env.DB_DATABASE as string,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT) || 1433,
-    options: {
-        encrypt: false,
-        trustServerCertificate: true
-        // trustedConnection: true
-    }
+  server: process.env.DB_SERVER as string,
+  database: process.env.DB_DATABASE as string,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT) || 1433,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+    // trustedConnection: true
+  },
 };
 
 export const connectDB = async () => {
-    try {
-        await sql.connect(dbConfig);
-        console.log('✅ Connected to SQL Server');
-    } catch (error) {
-        console.error('❌ Database connection failed:', error);
-    }
+  try {
+    await sql.connect(dbConfig);
+    console.log("✅ Connected to SQL Server");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+  }
 };
 
 export default sql;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import sql from './config/db';
-import { connectDB } from './config/db';
+import sql from "./config/db";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 const app = express();
@@ -14,22 +14,22 @@ app.use(cors());
 
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node.js + TypeScript backend!');
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js + TypeScript backend!");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
 
-app.get('/talent', async (req, res) => {
-    try {
-        const result = await sql.query(
-            "SELECT * FROM Talent WHERE TalentId = 'BACE066F-1E09-4423-A954-250072693A6D'"
-        );
-        let talent = result.recordset[0] || null; 
-        res.json(talent);
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching Talent', error });
-    }
-})
+app.get("/talent", async (req, res) => {
+  try {
+    const result = await sql.query(
+      "SELECT * FROM Talent WHERE TalentId = 'BACE066F-1E09-4423-A954-250072693A6D'"
+    );
+    let talent = result.recordset[0] || null;
+    res.json(talent);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching Talent", error });
+  }
+});
